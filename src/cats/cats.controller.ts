9e161a2184aaa0939d7e34f84156c42da6893f61
João Heckmann <joao.heckmann@mouts.info>
2021-03-17 20:46:03 -0300

    Creating custom HTTP code route and custom response header route

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,10 +1,26 @@
+import { Header, HttpCode } from '@nestjs/common';
 import { Controller, Get, Post, Req } from '@nestjs/common';
 
 @Controller('cats')
 export class CatsController {
   @Post()
   create(): string {
+    // The response status code is always 200 by default, except for POST requests which are 201.
     return 'This action adds a new cat';
+  }
+
+  @Post('customHttpCode')
+  @HttpCode(204)
+  createWithCustomHttpCode() {
+    // In case of you status code isn't static, you can inject @Res() and manipulate the response manually
+    return 'This action adds a new cat and returns 204 HTTP code';
+  }
+
+  @Post('customResponseHeader')
+  @Header('Cache-Control', 'none')
+  createWithCustomResponseHeader() {
+    // We could also set headers using res.header() directly with @Res()
+    return 'This action adds a new cat and returns a custom response header';
   }
 
   @Get()
