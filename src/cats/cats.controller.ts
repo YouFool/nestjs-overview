a8df42f161087fd052b07ab26c903930dcbe0d19
João Heckmann <joao.heckmann@mouts.info>
2021-03-17 21:12:47 -0300

    Accessing request payload on create routes

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,4 +1,5 @@
 import {
+  Body,
   Controller,
   Get,
   Header,
@@ -9,26 +10,30 @@ import {
   Req,
 } from '@nestjs/common';
 import { Observable, of } from 'rxjs';
+import { CreateCatDto } from '../dto/create-cat-dto';
 
 @Controller('cats')
 export class CatsController {
   @Post()
-  create(): string {
+  create(@Body() createCatDto: CreateCatDto): string {
     // The response status code is always 200 by default, except for POST requests which are 201.
+    console.log(createCatDto);
     return 'This action adds a new cat';
   }
 
   @Post('customHttpCode')
   @HttpCode(204)
-  createWithCustomHttpCode() {
+  createWithCustomHttpCode(@Body() createCatDto: CreateCatDto) {
     // In case of you status code isn't static, you can inject @Res() and manipulate the response manually
+    console.log(createCatDto);
     return 'This action adds a new cat and returns 204 HTTP code';
   }
 
   @Post('customResponseHeader')
   @Header('Cache-Control', 'none')
-  createWithCustomResponseHeader() {
+  createWithCustomResponseHeader(@Body() createCatDto: CreateCatDto) {
     // We could also set headers using res.header() directly with @Res()
+    console.log(createCatDto);
     return 'This action adds a new cat and returns a custom response header';
   }
 
@@ -36,6 +41,16 @@ export class CatsController {
   findAll(@Req() request: Request): string {
     console.log(request);
     return 'This action returns all cats';
+  }
+
+  @Get('async')
+  async findAllAsync(): Promise<any[]> {
+    return [];
+  }
+
+  @Get('asyncRxJs')
+  findAllRxJs(): Observable<any[]> {
+    return of([]);
   }
 
   @Get('wild*cat')
@@ -63,15 +78,5 @@ export class CatsController {
     // We can also specify the parameters beforehand in the function declaration
     console.log(catId);
     return `This action returns a cat with the id of #${catId}`;
-  }
-
-  @Get('async')
-  async findAllAsync(): Promise<any[]> {
-    return [];
-  }
-
-  @Get('asyncRxJs')
-  findAllRxJs(): Observable<any[]> {
-    return of([]);
   }
 }
