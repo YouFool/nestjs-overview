a5898b03c009e19acf7449440553e524efba3729
João Heckmann <joao.heckmann@mouts.info>
2021-03-29 11:08:45 -0300

    Let's bind a global Middleware to the application

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // We can also bind a functional Middleware to every registered route, this way:
+  // app.use(anotherLoggerMiddleware);
+
   await app.listen(3001);
 }
 bootstrap();
