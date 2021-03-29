e9c56db7b7318e8d9586ecc963051be83afd9266
João Heckmann <joao.heckmann@mouts.info>
2021-03-29 13:05:25 -0300

    Let's create a filter for all exceptions and bind it globally

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AllExceptionsFilter } from './all-exceptions.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,7 +10,7 @@ async function bootstrap() {
 
   // This way, we set a new global-scoped filter, but it cannot inject any dependencies
   // To use a global-scoped filter with dependencies, see app.module.ts
-  // app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalFilters(new AllExceptionsFilter());
 
   await app.listen(3001);
 }
