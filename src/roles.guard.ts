9c0c17706e22fb815b062bcb6e71366a8cff2d45
João Heckmann <joao.heckmann@mouts.info>
2021-03-31 11:06:14 -0300

    Let's add some logic to our RolesGuard to handle Role validation

--- src/roles.guard.ts
+++ src/roles.guard.ts
@@ -1,11 +1,23 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
-import { Observable } from 'rxjs';
+import { Reflector } from '@nestjs/core';
 
 @Injectable()
 export class RolesGuard implements CanActivate {
-  canActivate(
-    context: ExecutionContext,
-  ): boolean | Promise<boolean> | Observable<boolean> {
-    return true;
+  constructor(private reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const roles = this.reflector.get<string[]>('roles', context.getHandler);
+
+    if (!roles) {
+      return true;
+    }
+
+    const request = context.switchToHttp().getRequest();
+    const user = request.user;
+    return this.matchRoles(roles, user.roles);
+  }
+
+  matchRoles(roles: string[], roles2: any): boolean {
+    return roles === roles2;
   }
 }
